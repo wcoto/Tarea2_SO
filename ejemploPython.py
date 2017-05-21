c92b90dd55decc535bb18a6a73560203600375c9
Wagner Coto Meneses <wagner.coto.m@gmail.com>
2017-05-21 15:57:44 -0600

    Devuelve valor booleando si encuentra o no el ip dentro de la lista

--- ejemploPython.py
+++ ejemploPython.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-print ("Programa que lee archivo de texto\n")
+print ("Programa que compara host\n")
 fo = open("/carpetaDocker/mensaje.txt", "r+")
 str = fo.read(25);
 print "La informacion es : ", str
@@ -7,7 +7,10 @@ fo.close()
 
 def trustedHost(host):
 	with open("/carpetaDocker/Configuracion.config", "r+") as reader:
+		valor = False;
 		for line in reader:
-			print line.strip("\n") == host
+			if line.strip("\n") == host:
+				valor = True
+		return valor
 	
-trustedHost("365.254.21.4")
+print trustedHost("365.254.21.4")
