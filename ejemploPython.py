228323b9985ee53194bd30506edac333eb2544b5
Wagner Coto Meneses <wagner.coto.m@gmail.com>
2017-05-21 09:52:39 -0600

    Archivos iniciales en python

---
+++ ejemploPython.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python
+print ("programa que suma 2 numeros")
+#a = input("Ingrese el sumando 1: ")
+#b = input("Ingrese el sumando 2: ")
+#suma = a + b
+#print "El resultado de la suma es:", suma
