75d8e2eb71a3fe437e468132b3e81f14afd9fb09
Wagner Coto Meneses <wagner.coto.m@gmail.com>
2017-05-22 23:47:34 -0600

    RSA listo

--- ejemploPython.py
+++ ejemploPython.py
@@ -57,53 +57,20 @@ def concatMessage(message):
 		new.close()
 		print (message)	
 
-def generatePublicKey():
-    secret_code = "Unguessable"
-    key = RSA.generate(2048)
-    encrypted_key = key.exportKey(passphrase=secret_code, pkcs=1,
-                                    protection="scryptAndAES128-CBC")
-    file_out = open("/carpetaDocker/rsa_key.pem", "wb")
-    file_out.write(encrypted_key)
-
-
-def readPublicKey():
-    secret_code = "Unguessable"
-    encoded_key = open("/carpetaDocker/rsa_key.pem", "rb").read()
-    key = RSA.import_key(encoded_key, passphrase=secret_code)
-    print ("Public: ", key.publickey().exportKey())
-
-def encryptar():
-    RSAKey = RSA.generate(1024)
-    print("hoja")
-    publickey = RSAKey.publickey() # pub key export for exchange
-    print("inclusion")
-    cipher_rsa = PKCS1_OAEP.new(publickey)
-    encrypted = cipher_rsa.encrypt("hola".encode('utf-8'))
-    #message to encrypt is in the above line 'encrypt this message'
-
-    print ('encrypted message: ', encrypted) #ciphertext
-    cipher_rsa = PKCS1_OAEP.decrypt(encrypted)
-    decrypted = cipher_rsa
-    print ('\ndecrypted', decrypted)
-
-
-def encryptar2():
+def encryptar(message):
     publickey = RSA.generate(1024)
+    print ("Public: ", publickey.exportKey())
     cipher_rsa = PKCS1_OAEP.new(publickey)
-    encrypted = cipher_rsa.encrypt("hola".encode('utf-8'))
-    print ('encrypted message: ', encrypted)
+    encrypted = cipher_rsa.encrypt(message.encode('utf-8'))
+    print ('\nEncrypted message: ', encrypted)
     decrypted = cipher_rsa.decrypt(encrypted)
-    print ('\ndecrypted', decrypted)
+    print ('\nDecrypted message', decrypted)
 
-generatePublicKey()
-readPublicKey()
-encryptar2()
+encryptar()
 
 	
 #print trustedHost("365.254.21.4")
 #concatMessage("Uno Dos Tres Cuatro Cinco fin")
-#createServer_Client(int(os.environ.get('portServer')), "localhost")
-
 
 
 # docker build -t face .
